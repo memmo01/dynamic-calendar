78897f6da4503c260151721f8d898368fd9b32e0
michael emmons <memmons18@gmail.com>
2018-05-09 12:37:22 -0400

    added ability to update and delete specific event information. styled the edit buttons

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,6 +8,16 @@ module.exports=function(app){
         })
     })
 
+    app.get("/api/getSingleEvent/:id?",function(req,res){
+        db.events.findAll({
+            where:{
+                id:req.params.id
+            }
+        }).then(function(results){
+            res.json(results)
+        })
+    })
+
     app.get("/api/daterange/:start?/:end?",function(req,res){
         db.events.findAll({
             where:{
@@ -15,6 +25,29 @@ module.exports=function(app){
                     $gte:req.params.start,
                     $lte:req.params.end
                 }
+            }
+        }).then(function(results){
+            res.json(results)
+        })
+    })
+
+    app.put("/api/editEvent",function(req,res){
+        db.events.update({
+            time:req.body.time,
+            event:req.body.event
+        },{
+            where:{
+                id:req.body.id
+            }
+        }).then(function(results){
+            res.json(results)
+        })
+    })
+
+    app.delete("/api/deleteEvent",function(req,res){
+        db.events.destroy({
+            where:{
+                id:req.body.id
             }
         }).then(function(results){
             res.json(results)
